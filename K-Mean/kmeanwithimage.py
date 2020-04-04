ad3c7db1b63e51ed6bd39f22240aaafee94376af
duylebkHCM <duy.le.bku_16039@hcmut.edu.vn>
2020-04-04 19:40:58 +0700

    Update kmeanwithimage.py

--- K-Mean/kmeanwithimage.py
+++ K-Mean/kmeanwithimage.py
@@ -10,3 +10,15 @@ plt.show()
 print(img.shape)
 X = img.reshape((img.shape[0]*img.shape[1], img.shape[2]))
 
+for K in [2,5,10,15,20]:
+    kmeans = KMeans(n_clusters=K).fit(X)
+    labels = kmeans.predict(X)
+
+    img_1 = np.zeros_like(X)
+    for k in range(K):
+        img_1[labels == k] = kmeans.cluster_centers_[k]
+
+    img_2 =  img_1.reshape((img.shape[0], img.shape[1], img.shape[2]))
+    plt.imshow(img_2, interpolation = 'nearest')
+    plt.axis('off')
+    plt.show()
